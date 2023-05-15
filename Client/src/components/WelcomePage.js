6c5487d1f1594a4ff4c939ee0fa117012eeb4412
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-05-16 02:27:00 +0530

    Added backgroundImage

--- Client/src/components/WelcomePage.js
+++ Client/src/components/WelcomePage.js
@@ -1,7 +1,8 @@
+import image from '../assets/backgroundImage.jpg'
 
 function WelcomePage() {
   return (
-    <div>
+    <div style={{ backgroundImage:`url(${image})`,backgroundRepeat:"no-repeat", backgroundSize:"contain" , height:'100%',width:'100%'}}>
       <h1>Welcome Page</h1>
     </div>
   );
