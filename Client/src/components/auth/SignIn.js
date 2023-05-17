1987ddf1be650d619cbcf393568d3fd6c105c282
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-05-18 03:12:28 +0530

    Added form for enter email and password

--- Client/src/components/auth/SignIn.js
+++ Client/src/components/auth/SignIn.js
@@ -1,12 +1,45 @@
-import image from '../assets/backgroundImage.svg'
+import image from '../../assets/backgroundImage.svg'
 
-function WelcomePage() {
+function SignIn() {
   return (
-    <div style={{ backgroundImage:`url(${image})`,backgroundPosition: "center",backgroundRepeat:"no-repeat", backgroundSize:'cover' , height:'100vh',width:'100%', }}>
+    <div style={{ backgroundImage: `url(${image})`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: 'cover', height: '100vh', width: '100%', }}>
 
-      {/* <h1>Welcome Page</h1> */}
+      <form
+      // onSubmit={this.handleSubmit}
+      >
+        <h3>Sign In</h3>
+
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          // onChange={(e) => this.setState({ email: e.target.value })}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          // onChange={(e) => this.setState({ password: e.target.value })}
+          />
+        </div>
+
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          {/* Don't have an account? <a href="/sign-up">Sign Up</a> */}
+        </p>
+      </form>
     </div>
   );
 }
 
-export default WelcomePage;
+export default SignIn;
