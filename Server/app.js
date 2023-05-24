669027eacb646207e71a62c5d0ea4734b32d741f
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-05-25 02:04:11 +0530

    Added API for login

--- Server/app.js
+++ Server/app.js
@@ -46,6 +46,29 @@ app.post("/register", async (req, res) => {
     }
   });
 
+  app.post("/login-user", async (req, res) => {
+    const { email, password } = req.body;
+  
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ error: "User Not found" });
+    }
+    if (await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign({ email: user.email }, JWT_SECRET, {
+        // expiresIn: "15m",
+      });
+  
+      if (res.status(201)) {
+        return res.json({ status: "ok", data: token });
+      } else {
+        return res.json({ error: "error" });
+      }
+    }
+    res.json({ status: "error", error: "InvAlid Password" });
+  });
+
+
+
   app.listen(5000, () => {
     console.log("Server Started");
     });
