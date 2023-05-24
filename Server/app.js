2ac8eee7dd602cddeb72bd3c814551789243009b
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-05-25 02:03:42 +0530

    Added register API

---
+++ Server/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+const cors = require("cors")
+app.use(cors());
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
+
+const mongoUrl = "mongodb+srv://sanjanasolangaarachchi:Mu8wKnBqB4zdCU19@cluster0.zf7x54f.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((e) => console.log(e));
+
+require("./userDetails")
+
+const User = mongoose.model("UserInfo")
+app.post("/register", async (req, res) => {
+    const { fname, lname, email, password, userType } = req.body;
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    try {
+      const oldUser = await User.findOne({ email });
+  
+      if (oldUser) {
+        return res.json({ error: "User Exists" });
+        // alert("User exists")
+      }
+      await User.create({
+        fname,
+        lname,
+        email,
+        password: encryptedPassword,
+        userType,
+      });
+      res.send({ status: "ok" });
+    } catch (error) {
+      res.send({ status: "error" });
+    }
+  });
+
+  app.listen(5000, () => {
+    console.log("Server Started");
+    });
