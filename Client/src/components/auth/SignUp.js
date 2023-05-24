a794160fa5ac9aabdc0410c03d9c1b0cda717f40
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-05-25 01:56:59 +0530

    Added handleSubmit function

--- Client/src/components/auth/SignUp.js
+++ Client/src/components/auth/SignUp.js
@@ -1,93 +1,97 @@
-import image from '../../assets/backgroundImage.svg'
+import React, { useState } from "react";
 
-function SignUp() {
-    return (
-        <div style={{ backgroundImage: `url(${image})`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: 'cover', height: '100vh', width: '100%', }}>
+export default function SignUp() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userType, setUserType] = useState("");
+  const [secretKey, setSecretKey] = useState("");
 
-            <form 
-                // onSubmit={handleSubmit}
-            >
-                <h3>Sign Up</h3>
-                <div>
-                    Register As
-                    <input
-                        type="radio"
-                        name="UserType"
-                        value="User"
-                        // onChange={(e) => setUserType(e.target.value)}
-                    />
-                    User
-                    <input
-                        type="radio"
-                        name="UserType"
-                        value="Admin"
-                        // onChange={(e) => setUserType(e.target.value)}
-                    />
-                    Admin
-                </div>
-                {/* {userType == "Admin" ? (
-                    <div className="mb-3">
-                        <label>Secret Key</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Secret Key"
-                            // onChange={(e) => setSecretKey(e.target.value)}
-                        />
-                    </div>
-                ) : null} */}
+  const handleSubmit = (e) => {
+    
+    }
+  };
 
-                <div className="mb-3">
-                    <label>First name</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="First name"
-                        // onChange={(e) => setFname(e.target.value)}
-                    />
-                </div>
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <h3>Sign Up</h3>
+          <div>
+            Register As
+            <input
+              type="radio"
+              name="UserType"
+              value="User"
+              onChange={(e) => setUserType(e.target.value)}
+            />
+            User
+            <input
+              type="radio"
+              name="UserType"
+              value="Admin"
+              onChange={(e) => setUserType(e.target.value)}
+            />
+            Admin
+          </div>
+          {userType == "Admin" ? (
+            <div>
+              <label>Secret Key</label>
+              <input
+                type="text"
+                placeholder="Secret Key"
+                onChange={(e) => setSecretKey(e.target.value)}
+              />
+            </div>
+          ) : null}
 
-                <div className="mb-3">
-                    <label>Last name</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Last name"
-                        // onChange={(e) => setLname(e.target.value)}
-                    />
-                </div>
+          <div>
+            <label>First name</label>
+            <input
+              type="text"              
+              placeholder="First name"
+              onChange={(e) => setFname(e.target.value)}
+            />
+          </div>
 
-                <div className="mb-3">
-                    <label>Email address</label>
-                    <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Enter email"
-                        // onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
+          <div>
+            <label>Last name</label>
+            <input
+              type="text"
+              placeholder="Last name"
+              onChange={(e) => setLname(e.target.value)}
+            />
+          </div>
 
-                <div className="mb-3">
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        placeholder="Enter password"
-                        // onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
+          <div>
+            <label>Email address</label>
+            <input
+              type="email"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-                <div className="d-grid">
-                    <button type="submit" className="btn btn-primary">
-                        Sign Up
-                    </button>
-                </div>
-                <p className="forgot-password text-right">
-                    Already have an account <a href="/sign-in">Sign In</a>
-                </p>
-            </form>
-        </div>
-    );
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <button type="submit">
+              Sign Up
+            </button>
+          </div>
+          <p>
+            Already have an account <a href="/sign-in">Sign In</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
-
-export default SignUp;
