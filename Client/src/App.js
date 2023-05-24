927bfcc92ac3a505c9d69f4f59669fe53262de32
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-05-25 02:11:31 +0530

    Added paths

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import WelcomePage from '../src/components/auth/WelcomePage'
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import UserDetails from './components/auth/userDetails';
 
 const App = () => {
   return (
       <Router>
         <div>
             <Routes>
-              <Route exact path="/" element={<WelcomePage />} />
+              <Route exact path="/" element={<SignIn />} />
+              <Route path="/sign-in" element={<SignIn />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/userDetails" element={<UserDetails />} />
             </Routes>
         </div>
       </Router>
