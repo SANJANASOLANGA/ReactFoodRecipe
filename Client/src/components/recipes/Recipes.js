c4912d696376b5676f750dfdd277002d848d4e1c
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-07 11:53:59 +0530

    Added initial Recipes.js

---
+++ Client/src/components/recipes/Recipes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../Navbar";
+
+export default function Recipes() {
+  
+  return (
+    <div>
+      <NavBar/>
+      <h1>Recipes</h1>
+    </div>
+  );
+}
