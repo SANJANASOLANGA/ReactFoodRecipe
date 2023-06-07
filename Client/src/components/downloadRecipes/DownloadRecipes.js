aefef5dfaa322226f19957711cc9459180e2cb4b
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-07 12:31:25 +0530

    Imported meta

--- Client/src/components/downloadRecipes/DownloadRecipes.js
+++ Client/src/components/downloadRecipes/DownloadRecipes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../Navbar";
+import { meta } from "../contact/content_option";
 
 export default function DownloadRecipes() {
   
   return (
     <div>
       <NavBar/>
+      <title>{meta.title} | Download Recipes</title>
       <h1>downloadRecipes</h1>
     </div>
   );
