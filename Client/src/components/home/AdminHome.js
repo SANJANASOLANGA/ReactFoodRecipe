7ff38782bd044111630c854b467cb614d0ba75cd
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-07 11:25:16 +0530

    Imported NavBar

--- Client/src/components/home/AdminHome.js
+++ Client/src/components/home/AdminHome.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import NavBar from "../Navbar";
 export default function AdminHome() {
 
   return (
     <div>
+      <NavBar/>
       <h1>Welcome Admin</h1>
     </div>
   );
