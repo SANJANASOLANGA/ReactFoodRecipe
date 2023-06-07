25bb00490ed984514729ae61e4f2b580c7641db4
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-07 11:53:45 +0530

    Added initial downloadRecipes.js

---
+++ Client/src/components/downloadRecipes/DownloadRecipes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../Navbar";
+
+export default function DownloadRecipes() {
+  
+  return (
+    <div>
+      <NavBar/>
+      <h1>downloadRecipes</h1>
+    </div>
+  );
+}
