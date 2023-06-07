d67eff636595580dfd68e3819d672833252548a2
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-07 12:35:58 +0530

    Added title

--- Client/src/components/recipes/Recipes.js
+++ Client/src/components/recipes/Recipes.js
@@ -1,12 +1,20 @@
 import React from "react";
 import NavBar from "../Navbar";
+import { meta } from "../contact/content_option";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col, Alert } from "react-bootstrap";
 
 export default function Recipes() {
   
   return (
-    <div>
+    <HelmetProvider>
       <NavBar/>
-      <h1>Recipes</h1>
-    </div>
+      <Container>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{meta.title} | Recipes</title>
+        </Helmet>
+      </Container>
+    </HelmetProvider>
   );
 }
