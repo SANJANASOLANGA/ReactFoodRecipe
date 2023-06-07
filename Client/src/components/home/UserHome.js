66e7fd87157faac521dbab9ba9281126e722e35d
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-07 11:36:20 +0530

    Imported NavBar

--- Client/src/components/home/UserHome.js
+++ Client/src/components/home/UserHome.js
@@ -1,9 +1,11 @@
 import React from "react";
+import NavBar from "../Navbar";
 
 export default function UserHome() {
   
   return (
     <div>
+      <NavBar/>
       <h1>User Home</h1>
     </div>
   );
