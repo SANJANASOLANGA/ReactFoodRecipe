4226097862c0a11175795a256286fb4e14bc1b73
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-07 12:35:14 +0530

    Added title

--- Client/src/components/downloadRecipes/DownloadRecipes.js
+++ Client/src/components/downloadRecipes/DownloadRecipes.js
@@ -1,14 +1,20 @@
 import React from "react";
 import NavBar from "../Navbar";
 import { meta } from "../contact/content_option";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col, Alert } from "react-bootstrap";
 
 export default function DownloadRecipes() {
   
   return (
-    <div>
+    <HelmetProvider>
       <NavBar/>
-      <title>{meta.title} | Download Recipes</title>
-      <h1>downloadRecipes</h1>
-    </div>
+      <Container>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{meta.title} | Download Recipes</title>
+        </Helmet>
+      </Container>
+    </HelmetProvider>
   );
 }
