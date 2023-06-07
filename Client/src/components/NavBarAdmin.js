ad91db02a3a6317b60935611306e4fb96fd8954b
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-07 12:44:55 +0530

    Added initial NavBarAdmin.js

---
+++ Client/src/components/NavBarAdmin.js
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineFundProjectionScreen,
+} from "react-icons/ai";
+import { CgFileDocument, CgGitFork } from "react-icons/cg";
+import { ImPhone } from "react-icons/im";
+import { AiFillStar } from "react-icons/ai";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import logo from "../assets/logo.png";
+
+function NavBarAdmin() {
+  const [expand, setExpand] = useState(false);
+  const [navColour, setNavColour] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY >= 20) {
+      setNavColour(true);
+    } else {
+      setNavColour(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleToggle = () => setExpand(!expand);
+
+  const handleLinkClick = () => setExpand(false);
+
+  return (
+    <Navbar
+      expanded={expand}
+      fixed="top"
+      expand="md"
+      className={navColour ? "sticky" : "navbar"}
+    >
+      <Container>
+        <Navbar.Brand href="/adminhome" className="d-flex">
+          <img src={logo} className="img-fluid logo" alt="brand" />
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={handleToggle}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto" defaultActiveKey="#home">
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/adminhome"
+                onClick={handleLinkClick}
+              >
+                <AiOutlineHome style={{ marginBottom: "2px", color: 'red' }} />
+                Home
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/about"
+                onClick={handleLinkClick}
+              >
+                <AiOutlineUser style={{ marginBottom: "2px", color: 'red' }} />
+                About
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/recipes"
+                onClick={handleLinkClick}
+              >
+                <AiOutlineFundProjectionScreen
+                  style={{ marginBottom: "2px", color: 'red' }}
+                />
+                {" "}
+                Recipes
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/downloadRecipes"
+                onClick={handleLinkClick}
+              >
+                <CgFileDocument style={{ marginBottom: "2px", color: 'red' }} />
+                Download
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/contact"
+                onClick={handleLinkClick}
+              >
+                <ImPhone style={{ marginBottom: "2px", color: 'red' }} /> Contact
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item className="fork-btn">
+              <Button
+                href="https://github.com/SANJANASOLANGA"
+                target="_blank"
+                className="fork-btn-inner"
+              >
+                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                <AiFillStar style={{ fontSize: "1.1em", color: 'red' }} />
+              </Button>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBarAdmin;
