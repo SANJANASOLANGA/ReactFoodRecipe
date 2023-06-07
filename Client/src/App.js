2ec7e3c0b6f88309a29cfff3c17f92341d674c55
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-07 11:25:44 +0530

    Added Contact to Route

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import WelcomePage from '../src/components/auth/WelcomePage'
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import UserDetails from './components/auth/userDetails';
 import NavBar from './components/Navbar';
+import { Contact } from './components/contact/Contact';
 const App = () => {
   return (
       <Router>
         <div>
-          <NavBar />
             <Routes>
               <Route exact path="/" element={<SignIn />} />
               <Route path="/sign-in" element={<SignIn />} />
               <Route path="/sign-up" element={<SignUp />} />
               <Route path="/userDetails" element={<UserDetails />} />
+              <Route path='/contact-us' element={<Contact />} />
             </Routes>
         </div>
       </Router>
