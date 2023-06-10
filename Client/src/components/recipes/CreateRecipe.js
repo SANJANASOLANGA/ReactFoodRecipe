5de990b21c054fae3f42a9dd3144fbc5104c9918
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-11 01:23:32 +0530

    Created form

--- Client/src/components/recipes/CreateRecipe.js
+++ Client/src/components/recipes/CreateRecipe.js
@@ -1,52 +1,44 @@
 import React, { useState } from 'react';
+import { Box, Button, InputLabel, TextField, Typography } from '@mui/material';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import Form from './Form';
 
-const CreateReceipe = (props) => {
-  // Define the state with useState hook
-  const navigate = useNavigate();
-  const [receipeData, setReceipeData] = useState({
-    id: '',
-    receipeName: '',
-    ingredients: [],
-    description: '',
-    imageUrl: '',
-  });
-  const onChange = (e) => {
-    setReceipeData({ ...receipeData, [e.target.name]: e.target.value });
+const labelStyles = { mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' };
 
-    console.log(receipeData);
-  };
-  const onSubmit = (e) => {
-    axios
-      .post('https://hot-receipe.onrender.com/api/receipes', receipeData)
-      .then((res) => {
-        setReceipeData({
-          id: '',
-          receipeName: '',
-          ingredients: [],
-          description: '',
-          imageUrl: '',
-        });
+const CreateRecipe = () => {
 
-        // Push to /
-        navigate('/');
-      })
-      .catch((err) => {
-        console.log('Error in CreateReceipe!');
-      });
-  };
 
   return (
-    <Form
-      receipeData={receipeData}
-      onChange={onChange}
-      onSubmit={onSubmit}
-      setReceipeData={setReceipeData}
-      formType="create"
-    />
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Box
+          border={3}
+          borderColor="#29A0B1"
+          borderRadius={10}
+          boxShadow="10px 10px 20px #ccc"
+          padding={3}
+          margin="auto"
+          marginTop={3}
+          display="flex"
+          flexDirection="column"
+          width="80%"
+        >
+          <Typography fontWeight="bold" padding={3} color="grey" variant="h2" textAlign="center">
+            Add Your Recipe
+          </Typography>
+          <InputLabel sx={labelStyles}>Name</InputLabel>
+          <TextField name="title" onChange={handleChange} value={inputs.title} margin="normal" variant="outlined" />
+          <InputLabel sx={labelStyles}>Steps</InputLabel>
+          <TextField name="description" onChange={handleChange} value={inputs.description} margin="normal" variant="outlined" />
+          <InputLabel sx={labelStyles}>Image</InputLabel>
+          <TextField name="imageURL" onChange={handleChange} value={inputs.imageURL} margin="normal" variant="outlined" />
+          <Button sx={{ mt: 2, borderRadius: 4 }} variant="contained" color="warning" type="submit">
+            Submit
+          </Button>
+        </Box>
+      </form>
+    </div>
   );
 };
 
-export default CreateReceipe;
+export default CreateRecipe;
