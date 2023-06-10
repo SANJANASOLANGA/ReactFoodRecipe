81920453d5460d66f34aa06e61bbee04b7b90c1c
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-11 01:34:34 +0530

    Formatted the file

--- Client/src/components/recipes/Recipes.js
+++ Client/src/components/recipes/Recipes.js
@@ -3,31 +3,33 @@ import axios from 'axios';
 import UserRecipe from '../recipes/UserRecipe';
 
 const Recipes = () => {
-  const [recipes,setRecipes]= useState();
+  const [recipes, setRecipes] = useState([]);
   const sendRequest = async () => {
-    const res = await axios.get("http://localhost:5000/api/blog").catch(err=>console.log(err));
+    const res = await axios.get('http://localhost:5000/api/blog').catch((err) => console.log(err));
     const data = await res.data;
     return data;
-  }
-  useEffect(()=>{
-    sendRequest().then(data => setRecipes(data.blogs));
-  },[])
-  console.log(recipes);
-  
+  };
+
+  useEffect(() => {
+    sendRequest().then((data) => setRecipes(data.blogs));
+  }, []);
+
   return (
     <div>
-      {recipes && 
-      recipes.map((recipe,index) => (
-      <UserRecipe 
-      id = {recipe._id}
-      isUser = {localStorage.getItem("userId")===recipe.user._id}
-      title={recipe.title} 
-      description={recipe.description} 
-      imageURL={recipe.image} 
-      userName={recipe.user.name} /> 
-      ))}
+      {recipes &&
+        recipes.map((recipe, index) => (
+          <UserRecipe
+            key={index}
+            id={recipe._id}
+            isUser={localStorage.getItem('userId') === recipe.user._id}
+            title={recipe.title}
+            description={recipe.description}
+            imageURL={recipe.image}
+            userName={recipe.user.name}
+          />
+        ))}
     </div>
-  )
-}
+  );
+};
 
 export default Recipes;
