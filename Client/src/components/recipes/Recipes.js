d2ba1c0ab4206ba4516214f784539904fe1c356b
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-12 22:11:58 +0530

    Changed api

--- Client/src/components/recipes/Recipes.js
+++ Client/src/components/recipes/Recipes.js
@@ -5,7 +5,7 @@ import UserRecipe from '../recipes/UserRecipe';
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
   const sendRequest = async () => {
-    const res = await axios.get('http://localhost:5000/api/blog').catch((err) => console.log(err));
+    const res = await axios.get('http://localhost:5000/api/recipe').catch((err) => console.log(err));
     const data = await res.data;
     return data;
   };
