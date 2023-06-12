577057a7e18326043291630d044c08f44dd689c4
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-12 22:38:59 +0530

    Changed the API

--- Client/src/components/recipes/EditRecipe.js
+++ Client/src/components/recipes/EditRecipe.js
@@ -5,7 +5,7 @@ import AdminRecipe from '../recipes/AdminRecipe';
 const EditRecipes = () => {
   const [recipes, setRecipes] = useState([]);
   const sendRequest = async () => {
-    const res = await axios.get('http://localhost:5000/api/blog').catch((err) => console.log(err));
+    const res = await axios.get('http://localhost:5000/api/recipe').catch((err) => console.log(err));
     const data = await res.data;
     return data;
   };
