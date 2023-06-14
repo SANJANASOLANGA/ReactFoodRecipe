d8976495de97a612c803af32d3707f212302f316
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-14 22:42:05 +0530

    Added image and text

--- Client/src/components/home/UserHome.js
+++ Client/src/components/home/UserHome.js
@@ -3,17 +3,24 @@ import NavBar from "../Navbar";
 import { meta } from "../contact/content_option";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col, Alert } from "react-bootstrap";
+import mainUserHome from "../../assets/mainUserHome.jpeg";
 
 export default function UserHome() {
-  
+
   return (
     <HelmetProvider>
-      <NavBar/>
+      <NavBar />
       <Container>
         <Helmet>
           <meta charSet="utf-8" />
           <title>{meta.title} | Home</title>
         </Helmet>
+        <Container>
+          <div class="container-user">
+            <img src={mainUserHome} className="mainUserHome" alt="brand" />
+            <div class="mainUserHome-text">Let's Start Cooking With <br/> Popular Recipes </div>
+          </div>
+        </Container>
       </Container>
     </HelmetProvider>
   );
