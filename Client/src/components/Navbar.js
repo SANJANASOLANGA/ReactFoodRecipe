6c8a852fbdb3cb3e8daa6f9b2800e7dbe28b4bbe
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-14 18:02:46 +0530

    Removed the className attribute from the Navbar.Brand component

--- Client/src/components/Navbar.js
+++ Client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { AiOutlineHome, AiOutlineUser, } from "react-icons/ai";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
 import { CgFileAdd } from "react-icons/cg";
 import { ImPhone } from "react-icons/im";
 import { FiDownload } from "react-icons/fi";
@@ -9,7 +9,8 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import logo from "../assets/logo.png";
-import "../style.css"
+import "../style.css";
+
 function NavBar() {
   const [expand, setExpand] = useState(false);
   const [navColour, setNavColour] = useState(false);
@@ -27,8 +28,6 @@ function NavBar() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const handleToggle = () => setExpand(!expand);
-
   const handleLinkClick = () => setExpand(false);
 
   return (
@@ -40,68 +39,44 @@ function NavBar() {
     >
       <Container>
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto" defaultActiveKey="#home">
-              <Nav.Link
-                as={Link}
-                to="/userhome"
-                onClick={handleLinkClick}
-              >
-                <img src={logo} className="img-fluid logo" alt="brand" />
-              </Nav.Link>
-              <Nav.Link
-                as={Link}
-                to="/userhome"
-                onClick={handleLinkClick}
-              >
-                <AiOutlineHome style={{ marginBottom: "2px", color: 'red' }} />
-                Home
-              </Nav.Link>
-              <Nav.Link
-                as={Link}
-                to="/about"
-                onClick={handleLinkClick}
-              >
-                <AiOutlineUser style={{ marginBottom: "2px", color: 'red' }} />
-                About
-              </Nav.Link>
-              <Nav.Link
-                as={Link}
-                to="/recipes"
-                onClick={handleLinkClick}
-              >
-                <TbToolsKitchen2
-                  style={{ marginBottom: "2px", color: 'red' }}
-                />
-                {" "}
-                Recipes
-              </Nav.Link>
-              <Nav.Link
-                as={Link}
-                to="/downloadRecipes"
-                onClick={handleLinkClick}
-              >
-                <FiDownload style={{ marginBottom: "2px", color: 'red' }} />
-                Download
-              </Nav.Link>
-              <Nav.Link
-                as={Link}
-                to="/contact"
-                onClick={handleLinkClick}
-              >
-                <ImPhone style={{ marginBottom: "2px", color: 'red' }} /> Contact
-              </Nav.Link>
-              <Nav.Link
-                as={Link}
-                to="/sign-in"
-                onClick={handleLinkClick}
-              >
-                <CgFileAdd style={{ marginBottom: "2px", color: 'red' }} /> Add Recipe
-              </Nav.Link>
-          </Nav>
+          <div className="navbar-nav ml-auto">
+            <Navbar.Brand href="/userhome" className="d-flex">
+              <img src={logo} className="logo-image" alt="brand" />
+            </Navbar.Brand>
+
+            <Nav.Link as={Link} to="/userhome" onClick={handleLinkClick}>
+              <AiOutlineHome style={{ marginBottom: "2px", color: "red" }} />
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about" onClick={handleLinkClick}>
+              <AiOutlineUser style={{ marginBottom: "2px", color: "red" }} />
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/recipes" onClick={handleLinkClick}>
+              <TbToolsKitchen2
+                style={{ marginBottom: "2px", color: "red" }}
+              />
+              {" "}
+              Recipes
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/downloadRecipes"
+              onClick={handleLinkClick}
+            >
+              <FiDownload style={{ marginBottom: "2px", color: "red" }} />
+              Download
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact" onClick={handleLinkClick}>
+              <ImPhone style={{ marginBottom: "2px", color: "red" }} /> Contact
+            </Nav.Link>
+            <Nav.Link as={Link} to="/sign-in" onClick={handleLinkClick}>
+              <CgFileAdd style={{ marginBottom: "2px", color: "red" }} /> Add Recipe
+            </Nav.Link>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  
   );
 }
 
