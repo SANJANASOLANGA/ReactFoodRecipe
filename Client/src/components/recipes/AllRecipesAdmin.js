9f9eb6c34de312daedeee9cef7c167d2b017b565
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-15 23:10:48 +0530

    fetch the data from API

---
+++ Client/src/components/recipes/AllRecipesAdmin.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import UserRecipe from '../recipes/UserRecipe';
+
+const Recipes = () => {
+  const [recipes, setRecipes] = useState([]);
+  const sendRequest = async () => {
+    const res = await axios.get('http://localhost:5000/api/recipe').catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+
+  useEffect(() => {
+    sendRequest().then((data) => setRecipes(data.recipes));
+  }, []);
+
+  return (
+    <div>
+      {recipes &&
+        recipes.map((recipe, index) => (
+          <UserRecipe
+            key={index}
+            id={recipe._id}
+            isUser={localStorage.getItem('userId') === recipe.user._id}
+            title={recipe.title}
+            description={recipe.description}
+            imageURL={recipe.image}
+            userName={recipe.user.name}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default Recipes;
