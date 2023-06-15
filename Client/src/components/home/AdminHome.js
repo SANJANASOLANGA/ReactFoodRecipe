4648168c31ee345bddae74c00adaacd8f87d712d
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-15 23:17:14 +0530

    Added new images

--- Client/src/components/home/AdminHome.js
+++ Client/src/components/home/AdminHome.js
@@ -3,9 +3,9 @@ import NavBarAdmin from "../NavBarAdmin";
 import { meta } from "../contact/content_option";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col, Alert } from "react-bootstrap";
-import mainUserHome from "../../assets/mainUserHome.jpeg";
-import downloadRecipe from "../../assets/downloadRecipe.jpeg";
-import AddRecipe from "../../assets/AddRecipe.jpeg";
+import mainAdminHome from "../../assets/mainAdminHome.jpeg";
+import adminAddRecipe from "../../assets/adminAddRecipe.jpeg";
+import editRecipe from "../../assets/editRecipe.jpeg";
 
 export default function AdminHome() {
   
@@ -19,19 +19,19 @@ export default function AdminHome() {
         </Helmet>
         <Container>
           <div class="container-user">
-            <img src={mainUserHome} className="mainUserHome" alt="brand" />
+            <img src={mainAdminHome} className="mainUserHome" alt="brand" />
             <div class="mainUserHome-text">
               <a href="/recipes-admin">View Delicious Recipes <br/> From Around The World! </a>
             </div>
           </div>
           <div class="container-user-download">
-            <img src={downloadRecipe} className="downloadRecipe" alt="brand" />
+            <img src={adminAddRecipe} className="downloadRecipe" alt="brand" />
             <div class="downloadRecipe-text">
               <a href="/create-receipe">Add Your Own Recipes to Our Collection </a>
             </div>
           </div>
           <div class="container-user-add">
-            <img src={AddRecipe} className="addRecipe" alt="brand" />
+            <img src={editRecipe} className="addRecipe" alt="brand" />
             <div class="addRecipe-text">
               <a href="/edit-recipes">Edit and Improve <br/> Existing Recipes </a>
             </div>
