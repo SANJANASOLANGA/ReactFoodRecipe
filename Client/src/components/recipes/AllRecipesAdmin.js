6c85ecc744061a96d51b2cb5a4cc610ebb1e8472
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-16 13:51:06 +0530

    Added title

--- Client/src/components/recipes/AllRecipesAdmin.js
+++ Client/src/components/recipes/AllRecipesAdmin.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import UserRecipe from '../recipes/UserRecipe';
 import NavBarAdmin from '../NavBarAdmin';
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { meta } from "../contact/content_option";
+import { Container } from "react-bootstrap";
 
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
@@ -16,21 +19,30 @@ const Recipes = () => {
   }, []);
 
   return (
-    <div>
-      <NavBarAdmin/>
-      {recipes &&
-        recipes.map((recipe, index) => (
-          <UserRecipe
-            key={index}
-            id={recipe._id}
-            isUser={localStorage.getItem('userId') === recipe.user._id}
-            title={recipe.title}
-            description={recipe.description}
-            imageURL={recipe.image}
-            userName={recipe.user.name}
-          />
-        ))}
-    </div>
+    <HelmetProvider>
+      <NavBarAdmin />
+      <Container>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{meta.title} | Recipes</title>
+        </Helmet>
+      </Container>
+
+      <div>
+        {recipes &&
+          recipes.map((recipe, index) => (
+            <UserRecipe
+              key={index}
+              id={recipe._id}
+              isUser={localStorage.getItem('userId') === recipe.user._id}
+              title={recipe.title}
+              description={recipe.description}
+              imageURL={recipe.image}
+              userName={recipe.user.name}
+            />
+          ))}
+      </div>
+    </HelmetProvider>
   );
 };
 
