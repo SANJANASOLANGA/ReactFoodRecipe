7fe42fc8412d4aeeee4b9139960377fd771b667e
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-16 13:50:48 +0530

    Added title

--- Client/src/components/recipes/CreateRecipe.js
+++ Client/src/components/recipes/CreateRecipe.js
@@ -3,6 +3,9 @@ import { Box, Button, InputLabel, TextField, Typography } from '@mui/material';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import NavBarAdmin from '../NavBarAdmin';
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { meta } from "../contact/content_option";
+import { Container } from "react-bootstrap";
 
 const labelStyles = { mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' };
 
@@ -29,7 +32,7 @@ const CreateRecipe = () => {
       user: localStorage.getItem('userId'),
     }).catch((err) => console.log(err));
     const data = await res.data;
-    alert('Successfully data added!',data)
+    alert('Successfully data added!', data)
     return data;
   };
 
@@ -40,36 +43,44 @@ const CreateRecipe = () => {
   };
 
   return (
-    <div>
-      <NavBarAdmin/>
-      <form onSubmit={handleSubmit}>
-        <Box
-          border={3}
-          borderColor="#29A0B1"
-          borderRadius={10}
-          boxShadow="10px 10px 20px #ccc"
-          padding={3}
-          margin="auto"
-          marginTop={3}
-          display="flex"
-          flexDirection="column"
-          width="80%"
-        >
-          <Typography fontWeight="bold" padding={3} color="grey" variant="h2" textAlign="center">
-            Add Your Recipe
-          </Typography>
-          <InputLabel sx={labelStyles}>Name</InputLabel>
-          <TextField name="title" onChange={handleChange} value={inputs.title} margin="normal" variant="outlined" />
-          <InputLabel sx={labelStyles}>Steps</InputLabel>
-          <TextField name="description" onChange={handleChange} value={inputs.description} margin="normal" variant="outlined" />
-          <InputLabel sx={labelStyles}>Image</InputLabel>
-          <TextField name="imageURL" onChange={handleChange} value={inputs.imageURL} margin="normal" variant="outlined" />
-          <Button sx={{ mt: 2, borderRadius: 4 }} variant="contained" color="warning" type="submit">
-            Submit
-          </Button>
-        </Box>
-      </form>
-    </div>
+    <HelmetProvider>
+      <NavBarAdmin />
+      <Container>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{meta.title} | Add Recipes</title>
+        </Helmet>
+      </Container>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <Box
+            border={3}
+            borderColor="#29A0B1"
+            borderRadius={10}
+            boxShadow="10px 10px 20px #ccc"
+            padding={3}
+            margin="auto"
+            marginTop={3}
+            display="flex"
+            flexDirection="column"
+            width="80%"
+          >
+            <Typography fontWeight="bold" padding={3} color="grey" variant="h2" textAlign="center">
+              Add Your Recipe
+            </Typography>
+            <InputLabel sx={labelStyles}>Name</InputLabel>
+            <TextField name="title" onChange={handleChange} value={inputs.title} margin="normal" variant="outlined" />
+            <InputLabel sx={labelStyles}>Steps</InputLabel>
+            <TextField name="description" onChange={handleChange} value={inputs.description} margin="normal" variant="outlined" />
+            <InputLabel sx={labelStyles}>Image</InputLabel>
+            <TextField name="imageURL" onChange={handleChange} value={inputs.imageURL} margin="normal" variant="outlined" />
+            <Button sx={{ mt: 2, borderRadius: 4 }} variant="contained" color="warning" type="submit">
+              Submit
+            </Button>
+          </Box>
+        </form>
+      </div>
+    </HelmetProvider>
   );
 };
 
