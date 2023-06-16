6267b0c5e68548e3ba908ca0767b7e24cbef5d77
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-16 20:45:04 +0530

    Edited the contactConfig details

--- Client/src/components/contact/content_option.js
+++ Client/src/components/contact/content_option.js
@@ -3,9 +3,9 @@ const meta = {
 };
 
 const contactConfig = {
-    YOUR_EMAIL: "sanjanasolangaarachchi@gmail.com",
-    YOUR_FONE: "+94719640127",
-    description: "If you have any questions or would like to know more information, please feel free to contact me through the email or phone number provided. You can also leave me a short message and I'll get back to you as soon as possible. I'm always happy to provide additional information. Thank you for your time!",
+    YOUR_EMAIL: "cookerycorner@gmail.com",
+    YOUR_FONE: "+94112916610",
+    description: "If you have any questions or would like to know more information about recipes, please feel free to contact us through the email or phone number provided. You can also leave us a short message and We'll get back to you as soon as possible. We're always happy to provide additional information about recipes. Thank you for your time!",
     YOUR_SERVICE_ID: "service_hy8mzwp",
     YOUR_TEMPLATE_ID: "template_34n18yu",
     YOUR_USER_ID: "ydXDVQqTmDpo1RhPB",
