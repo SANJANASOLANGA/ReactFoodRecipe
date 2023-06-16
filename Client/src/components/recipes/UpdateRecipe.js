43a31cc727b08a0f135c013b42b893b867573f49
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-17 03:00:25 +0530

    Added styles for form

--- Client/src/components/recipes/UpdateRecipe.js
+++ Client/src/components/recipes/UpdateRecipe.js
@@ -2,40 +2,43 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios';
 import { Box, Button, InputLabel, TextField, Typography } from '@mui/material';
-const labelStyles ={mb: 1,mt:2,fontSize:'24px', fontWeight:'bold'}
-const BlogDetail = () => {
+
+const labelStyles = { color: 'white', mb: 1, mt: 2, fontSize: '20px' };
+
+const RecipeDetail = () => {
   const navigate = useNavigate();
   const [blog, setBlog] = useState();
   const id = useParams().id;
   console.log(id);
   const [inputs, setInputs] = useState(
-    
+
   )
- 
-  const handleChange = (e)=>{
-      setInputs((prevState)=>({
-          ...prevState,[e.target.name]: e.target.value
-      }));
-    
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState, [e.target.name]: e.target.value
+    }));
+
 
   }
   const fetchDetails = async () => {
-    const res = await axios.get(`http://localhost:5000/api/recipe/${id}`).catch(err=>console.log(err))
-    const data =await res.data;
+    const res = await axios.get(`http://localhost:5000/api/recipe/${id}`).catch(err => console.log(err))
+    const data = await res.data;
     return data;
   }
 
-  useEffect(()=>{
-       fetchDetails().then(data=>{setBlog(data.blog)
-      setInputs({title:data.blog.title,description:data.blog.description})
-      })
-  },[id]);
+  useEffect(() => {
+    fetchDetails().then(data => {
+      setBlog(data.blog)
+      setInputs({ title: data.blog.title, description: data.blog.description })
+    })
+  }, [id]);
   const sendRequest = async () => {
-    const res = await axios.put(`http://localhost:5000/api/recipe/update/${id}`,{
+    const res = await axios.put(`http://localhost:5000/api/recipe/update/${id}`, {
       title: inputs.title,
       description: inputs.description
 
-    }).catch(err=>console.log(err))
+    }).catch(err => console.log(err))
     const data = await res.data;
     return data;
   }
@@ -43,37 +46,37 @@ const BlogDetail = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(inputs);
-    sendRequest().then((data) => console.log(data)).then(()=>navigate("/edit-recipes"));
+    sendRequest().then((data) => console.log(data)).then(() => navigate("/edit-recipes"));
   }
   return (
-    <div>
-      {inputs && 
-      <form onSubmit={handleSubmit}>
-    <Box
-    border={3}
-    borderColor="#29A0B1"
-    borderRadius={10}
-    boxShadow="10px 10px 20px #ccc"
-    padding={3}
-    margin={"auto"}
-    marginTop={3}
-    display="flex"
-    flexDirection={"column"}
-    width={"80%"}
-    >
-      <Typography fontWeight={'bold'} padding={3} color="grey" variant='h2' textAlign={'center'}>Update Your Blog</Typography>
-      <InputLabel sx={labelStyles}>Title</InputLabel>
-      <TextField name="title" onChange={handleChange} value={inputs.title} margin={'normal'} variant="outlined"/>
-      <InputLabel sx={labelStyles}>Description</InputLabel>
-      <TextField  name="description" onChange={handleChange} value={inputs.description} margin={'normal'} variant="outlined"/>
-   
-      <Button sx={{mt:2,borderRadius:4}} variant="contained" color="warning" type='submit'>Submit</Button>
-
-    </Box>
-    </form>
-} 
-</div>
+    <div className='auth'>
+      {inputs &&
+        <form className="form-container" onSubmit={handleSubmit}>
+          <Box
+            border={3}
+            borderRadius={10}
+            padding={3}
+            margin="auto"
+            marginTop={3}
+            display="flex"
+            flexDirection="column"
+            width="80%"
+          >
+            <Typography><h3 className="form-container__title">Update Your Recipe</h3></Typography>
+            <InputLabel sx={labelStyles}>Recipe Name</InputLabel>
+            <TextField style={{color:"white"}} name="title" onChange={handleChange} value={inputs.title} margin={'normal'} variant="outlined" />
+            <InputLabel sx={labelStyles}>Steps</InputLabel>
+            <TextField name="description" onChange={handleChange} value={inputs.description} margin={'normal'} variant="outlined" />
+            <div style={{ display: "flex", position: 'relative', justifyContent: 'center', alignItems: 'center' }}>
+              <Button className='form-container__submit-button' style={{ width: '30%', height: '27px' }} variant="contained" type="submit">
+                Submit
+              </Button>
+            </div>
+          </Box>
+        </form>
+      }
+    </div>
   )
 }
 
-export default BlogDetail
+export default RecipeDetail
