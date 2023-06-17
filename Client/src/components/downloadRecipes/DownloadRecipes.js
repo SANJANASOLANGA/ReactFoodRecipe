85a66fe0d597f77b7c08ae9cb75b845fd94094bd
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-17 15:44:32 +0530

    Added pdf for download and updated the downloadRecipe function

--- Client/src/components/downloadRecipes/DownloadRecipes.js
+++ Client/src/components/downloadRecipes/DownloadRecipes.js
@@ -5,28 +5,31 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container } from "react-bootstrap";
 import { AiOutlineDownload } from "react-icons/ai";
 import img from '../../assets/logo.png'
+import pdf1 from '../../assets/Document.pdf';
+import pdf2 from '../../assets/Document1.pdf';
+import pdf3 from '../../assets/Document2.pdf';
 
 export default function DownloadRecipes() {
   const images = [
-    img,
-    img,
-    img,
-    img,
-    img,
-    img,
-    img,
-    img,
-    img,
-    // 'image4.jpg',
-    // 'image5.jpg',
-    // 'image6.jpg',
-    // 'image7.jpg',
-    // 'image8.jpg',
-    // 'image9.jpg',
+    { img: img, pdf: pdf1 },
+    { img: img, pdf: pdf2 },
+    { img: img, pdf: pdf3 },
+    { img: img, pdf: pdf1 },
+    { img: img, pdf: pdf2 },
+    { img: img, pdf: pdf3 },
+    { img: img, pdf: pdf1 },
+    { img: img, pdf: pdf2 },
+    { img: img, pdf: pdf3 },
   ];
 
-  const downloadRecipe = () => {
+  const downloadRecipe = (pdf) => {
     console.log('Recipe downloaded!');
+    const link = document.createElement('a');
+    link.href = pdf;
+    link.download = 'recipe.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
 
   return (
@@ -41,8 +44,8 @@ export default function DownloadRecipes() {
       <div className="image-grid">
         {images.map((image, index) => (
           <div key={index} className="image-container">
-            <img src={image} alt={`Image ${index + 1}`} className="image" />
-            <button className="download-button" onClick={downloadRecipe}><AiOutlineDownload />{' '}Download Recipe</button>
+            <img src={image.img} alt={`Image ${index + 1}`} className="image" />
+            <button className="download-button" onClick={() => downloadRecipe(image.pdf)}><AiOutlineDownload />{' '}Download Recipe</button>
           </div>
         ))}
       </div>
