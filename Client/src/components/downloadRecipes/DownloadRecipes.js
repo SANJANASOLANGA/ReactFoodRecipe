99e83f7dc7ced0b533abc1a881cffd89c8ec0511
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-17 13:29:56 +0530

    Added image and button

--- Client/src/components/downloadRecipes/DownloadRecipes.js
+++ Client/src/components/downloadRecipes/DownloadRecipes.js
@@ -2,19 +2,45 @@ import React from "react";
 import NavBar from "../Navbar";
 import { meta } from "../contact/content_option";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { Container, Row, Col, Alert } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import img from '../../assets/logo.png'
 
 export default function DownloadRecipes() {
-  
+  const images = [
+    img,
+    img,
+    img,
+    img,
+    img,
+    img,
+    img,
+    img,
+    img,
+    // 'image4.jpg',
+    // 'image5.jpg',
+    // 'image6.jpg',
+    // 'image7.jpg',
+    // 'image8.jpg',
+    // 'image9.jpg',
+  ];
+
   return (
     <HelmetProvider>
-      <NavBar/>
+      <NavBar />
       <Container>
         <Helmet>
           <meta charSet="utf-8" />
           <title>{meta.title} | Download Recipes</title>
         </Helmet>
       </Container>
+      <div className="image-grid">
+        {images.map((image, index) => (
+          <div key={index} className="image-container">
+            <img src={image} alt={`Image ${index + 1}`} className="image" />
+            <button className="download-button">Download Recipe</button>
+          </div>
+        ))}
+      </div>
     </HelmetProvider>
   );
 }
