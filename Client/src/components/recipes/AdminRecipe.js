16e096c3a7457e531b12cd0a8f91a9f8a2de485d
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-18 16:12:59 +0530

    Added width and height

--- Client/src/components/recipes/AdminRecipe.js
+++ Client/src/components/recipes/AdminRecipe.js
@@ -55,7 +55,7 @@ const AdminRecipe = ({ title, description, imageURL, id }) => {
           title={title}
           style={{color: 'white'}}
         />
-        <CardMedia style={{ height: "250px", paddingTop: "2%", color: 'white', opacity:1 }}component="img" image={imageURL} alt="Recipe" title={title} />
+        <CardMedia style={{ width: "100%", height: "100%", paddingTop: "2%", color: 'white', opacity:1, objectFit: 'cover' }}component="img" image={imageURL} alt="Recipe" title={title} />
         <CardContent>
           <Typography variant="body2" color="text.secondary">
             {bulletPoints.map((point, index) => (
