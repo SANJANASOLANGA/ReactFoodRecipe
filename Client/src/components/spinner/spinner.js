77a41cc6be3f0780d2f7447cc0d47a26c384ce42
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-18 14:43:57 +0530

    Added initial spinner.js

---
+++ Client/src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css'; 
+
+const Spinner = () => {
+  return (
+    <div className="spinner-container">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
