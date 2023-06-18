f150710f06c1822384dc7ebdad319b7b2bd0acd7
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-06-18 13:29:33 +0530

    Added title

--- Client/src/components/auth/SignUp.js
+++ Client/src/components/auth/SignUp.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { meta } from "../contact/content_option";
+import { Container } from 'react-bootstrap';
 
 export default function SignUp() {
     const navigate = useNavigate();
@@ -53,66 +56,76 @@ export default function SignUp() {
     };
 
     return (
-        <div>
-            <div className='auth'>
-                <form className="form-container" onSubmit={handleSubmit}>
-                    <h6 className="form-container__subtitle">Are you a admin? Please Sign Up first to Add Recipe</h6> <br /><br />
-                    <h3 className="form-container__title">Sign Up</h3>
-                    <div>
-                        <label className="form-container__label">Secret Key</label>
-                        <input
-                            type="text"
-                            placeholder="Secret Key"
-                            onChange={(e) => setSecretKey(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label className="form-container__label">First name</label>
-                        <input
-                            type="text"
-                            placeholder="First name"
-                            onChange={(e) => setFname(e.target.value)}
-                        />
-                    </div>
+        <HelmetProvider>
+            <Container>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>{meta.title} | Sign Up</title>
+                </Helmet>
+            </Container>
+            <Container>
+                <div>
+                    <div className='auth'>
+                        <form className="form-container" onSubmit={handleSubmit}>
+                            <h6 className="form-container__subtitle">Are you a admin? Please Sign Up first to Add Recipe</h6> <br /><br />
+                            <h3 className="form-container__title">Sign Up</h3>
+                            <div>
+                                <label className="form-container__label">Secret Key</label>
+                                <input
+                                    type="text"
+                                    placeholder="Secret Key"
+                                    onChange={(e) => setSecretKey(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <label className="form-container__label">First name</label>
+                                <input
+                                    type="text"
+                                    placeholder="First name"
+                                    onChange={(e) => setFname(e.target.value)}
+                                />
+                            </div>
 
-                    <div>
-                        <label className="form-container__label">Last name</label>
-                        <input
-                            type="text"
-                            placeholder="Last name"
-                            onChange={(e) => setLname(e.target.value)}
-                        />
-                    </div>
+                            <div>
+                                <label className="form-container__label">Last name</label>
+                                <input
+                                    type="text"
+                                    placeholder="Last name"
+                                    onChange={(e) => setLname(e.target.value)}
+                                />
+                            </div>
 
-                    <div>
-                        <label className="form-container__label">Email address</label>
-                        <input
-                            type="email"
-                            placeholder="Enter email"
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
+                            <div>
+                                <label className="form-container__label">Email address</label>
+                                <input
+                                    type="email"
+                                    placeholder="Enter email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </div>
 
-                    <div>
-                        <label className="form-container__label">Password</label>
-                        <input
-                            type="password"
-                            placeholder="Enter password"
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
+                            <div>
+                                <label className="form-container__label">Password</label>
+                                <input
+                                    type="password"
+                                    placeholder="Enter password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
 
-                    <div>
-                        <br />
-                        <button type="submit" className="form-container__submit-button">
-                            Sign Up
-                        </button>
+                            <div>
+                                <br />
+                                <button type="submit" className="form-container__submit-button">
+                                    Sign Up
+                                </button>
+                            </div>
+                            <p>
+                                Already have an account? <a href="/sign-in"><span style={{ color: "white" }}>Sign In</span></a>
+                            </p>
+                        </form>
                     </div>
-                    <p>
-                        Already have an account? <a href="/sign-in"><span style={{ color: "white" }}>Sign In</span></a>
-                    </p>
-                </form>
-            </div>
-        </div>
+                </div>
+            </Container>
+        </HelmetProvider>
     );
 }
